a19338ce349877d1a4c0e3d7da1fec5df9cfef1b
Tauwasser <tauwasser@tauwasser.eu>
2015-05-29 22:27:12 +0200

    MBC2: add standard tests
    Signed-off-by: Tauwasser <tauwasser@tauwasser.eu>

--- MBC_Test/MBC_Test.c
+++ MBC_Test/MBC_Test.c
@@ -1,31 +1,32 @@
-/*           7          6        5       4       3       2       1       0 
- * Port A:  D4         D3       D2      D1      D0      A15     A14     A13
- * Port B:                                                              LED
- * Port C:  #RAM_CS    AA14    AA13    RA18    RA17    RA16    RA15    RA14
- * Port D:   RAM_CS #ROM_CS  #RESET
- * Port E:                                             nWR     nRD     nCS 
+/*           7         6        5       4       3       2       1       0 
+ * Port A:  A15       A14      A8              D3      D2      D1      D0 
+ * Port B:                                                             LED
+ * Port C:  #ROM_CS #RESET     A7      A6     RA17    RA16    RA15    RA14
+ * Port D:  A5        A4       A3      A2      A1      A0     TXD     RXD 
+ * Port E:                                            nWR     nRD     nCS 
  *
  */
 
-#define A_MASK  (1u << PA2 | 1u << PA1 | 1u << PA0)
-#define A_SHIFT PA0
+#define A_HI_MASK  (1u << PA7 | 1u << PA6 | 1u << PA5)
+#define A_HI_SHIFT PA5
 
-#define D_MASK  (1u << PA7 | 1u << PA6 | 1u << PA5 | 1u << PA4 | 1u << PA3)
-#define D_SHIFT PA3
+#define D_MASK  (1u << PA3 | 1u << PA2 | 1u << PA1 | 1u << PA0)
+#define D_SHIFT PA0
 
-#define RA_MASK  (1u << PC4 | 1u << PC3 | 1u << PC2 | 1u << PC1 | 1u << PC0)
+#define RA_MASK  (1u << PC3 | 1u << PC2 | 1u << PC1 | 1u << PC0)
 #define RA_SHIFT PC0
-#define AA_MASK  (1u << PC6 | 1u << PC5)
-#define AA_SHIFT PC5
-#define nRAM_CS  (1u << PC7)
+#define A_MID_MASK  (1u << PC5 | 1u << PC4)
+#define A_MID_SHIFT PC4
+
+#define nROM_CS  (1u << PC7)
+#define nRST     (1u << PC6)
 
 #define nWR (1u << PE2)
 #define nRD (1u << PE1)
 #define nCS (1u << PE0)
 
-#define nRST    (1u << PD5)
-#define nROM_CS (1u << PD6)
-#define RAM_CS  (1u << PD7)
+#define A_LO_MASK  (1u << PD7 | 1u << PD6 | 1u << PD5 | 1u << PD4 | 1u << PD3 | 1u << PD2)
+#define A_LO_SHIFT PD2
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
@@ -60,57 +61,56 @@ inline void deassertRD(void) {
 }
 
 inline void assertRST(void) {
-	IOPORTD->out &= ~nRST;
-	IOPORTD->dir |= nRST;
+	IOPORTC->out &= ~nRST;
+	IOPORTC->dir |= nRST;
 }
 
 inline void deassertRST(void) {
-	IOPORTD->dir &= ~nRST;
-	IOPORTD->out |= nRST;
+	IOPORTC->dir &= ~nRST;
+	IOPORTC->out |= nRST;
 }
 
 inline void putAddr(uint16_t addr) {
-	IOPORTA->out = (IOPORTA->out & D_MASK) | ((addr >> 13) & A_MASK);
+	uint8_t addr_tmp = (addr >> 13) & 0x06;
+	if (addr & 0x0100)
+		addr_tmp |= 0x01u;
+	
+	IOPORTD->out = ((addr << A_LO_SHIFT) & A_LO_MASK) | (IOPORTD->out & ~A_LO_MASK);
+	IOPORTC->out &= ~A_MID_MASK;
+	IOPORTC->out |= ((addr >> 6) << A_MID_SHIFT) & A_MID_MASK;
+	IOPORTA->out = (IOPORTA->out & D_MASK) | ((addr_tmp << A_HI_SHIFT) & A_HI_MASK);
 }
 
 inline void putAddrCS(uint16_t addr) {
-	IOPORTA->out = (IOPORTA->out & D_MASK) | ((addr >> 13) & A_MASK);
+	putAddr(addr);
 	deassertCS();
 	if (addr >= 0xA000u && addr <= 0xFF00u)
 		assertCS();
 }
 
 inline void putData(uint8_t data) {
-	IOPORTA->out = ((data << D_SHIFT) & D_MASK) | (IOPORTA->out & A_MASK);
-	IOPORTA->dir = D_MASK | A_MASK;
+	IOPORTA->out = ((data << D_SHIFT) & D_MASK) | (IOPORTA->out & A_HI_MASK);
+	IOPORTA->dir = D_MASK | A_HI_MASK;
 };
 
 inline void floatData(void) {
-	IOPORTA->out = D_MASK | (IOPORTA->out & A_MASK);
-	IOPORTA->dir = A_MASK;
+	IOPORTA->out = D_MASK | (IOPORTA->out & A_HI_MASK);
+	IOPORTA->dir = A_HI_MASK;
 }
 
 inline uint8_t getRA(void) {
 	return (IOPORTC->in & RA_MASK) >> RA_SHIFT;
 }
 
-inline uint8_t getAA(void) {
-	return (IOPORTC->in & AA_MASK) >> AA_SHIFT;
-}
-
-inline uint8_t getnRAM_CS(void) {
-	return (IOPORTC->in & nRAM_CS);
-}
-
-inline uint8_t getRAM_CS(void) {
-	return (IOPORTD->in & RAM_CS);
-}
-
 inline uint8_t getnROM_CS(void) {
-	return (IOPORTD->in & nROM_CS);
+	return (IOPORTC->in & nROM_CS);
 }
 
-void writeMBC1(uint16_t addr, uint8_t data) {
+inline uint8_t getData(void) {
+	return (IOPORTA->in & D_MASK) >> D_SHIFT;
+}
+
+void writeMBC2(uint16_t addr, uint8_t data) {
 	
 	putAddrCS(addr);
 	putData(data);
@@ -123,6 +123,24 @@ void writeMBC1(uint16_t addr, uint8_t data) {
 	
 }
 
+uint8_t readMBC2(uint16_t addr) {
+	
+	uint8_t tmp;
+	
+	putAddrCS(addr);
+	floatData();
+	
+	assertRD();
+	delay_us(1);
+	tmp = getData();
+	deassertRD();
+	deassertCS();
+	floatData();
+	
+	return tmp;
+	
+}
+
 void write_usart_hex(const uint8_t num) {
 	
 	const char alpha[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
@@ -132,10 +150,8 @@ void write_usart_hex(const uint8_t num) {
 	
 }
 
-void printMBC1Status(void) {
+void printMBC2Status(void) {
 	
-	uart0Puts("AA: 0x");
-	write_usart_hex(getAA());
 	uart0Puts(" RA: 0x");
 	write_usart_hex(getRA());
 	uart0Puts("\n");
@@ -144,13 +160,6 @@ void printMBC1Status(void) {
 void printnROM_CS(void){
 	uart0Puts(" /// nROM_CS: ");
 	getnROM_CS() ? uart0Puts("1") : uart0Puts("0");
-}
-
-void print_bothRAM_CS(void){
-	uart0Puts(" /// nRAM_CS: ");
-	getnRAM_CS() ? uart0Puts("1") : uart0Puts("0");
-	uart0Puts(" RAM_CS: ");
-	getRAM_CS() ? uart0Puts("1") : uart0Puts("0");
 }
 
 void printCS_Data(void(*fun)(void)) {
@@ -181,24 +190,16 @@ void printCS_Data(void(*fun)(void)) {
 	
 }
 
-const char str0[] PROGMEM = "Drive nROM_CS '0', RAM_CS '1'.\n";
-const char str1[] PROGMEM = "Float RAM_CS, nROM_CS.\n";
-const char str2[] PROGMEM = "Disable RAM, RAM Bank 0x01, Mode 0 (16Mb/8kB)\n";
-const char str3[] PROGMEM = "Enable RAM, RAM Bank 0x01, Mode 0 (16Mb/8kB)\n";
-const char str4[] PROGMEM = "Disable RAM, RAM Bank 0x01, Mode 1 (4Mb/32kB)\n";
-const char str5[] PROGMEM = "Enable RAM, RAM Bank 0x01, Mode 1 (4Mb/32kB)\n";
-const char str6[] PROGMEM = "Latch Test\n";
-const char str7[] PROGMEM = "Reg vs Latch\n";
+const char str0[] PROGMEM = "Drive nROM_CS '0'.\n";
+const char str1[] PROGMEM = "Float nROM_CS.\n";
+const char str2[] PROGMEM = "Init RAM.\n";
+const char str3[] PROGMEM = "Print RAM.\n";
 
 PGM_P const str_tbl[] PROGMEM = {
 	str0,
 	str1,
 	str2,
 	str3,
-	str4,
-	str5,
-	str6,
-	str7,
 };
 
 void uart0Puts_p(const uint8_t ix) {
@@ -232,21 +233,21 @@ int main(void)
 		  0 |   0 |   0,
 	};
 	
-	// PU on D4..D0, drive A15..A13
-	IOPORTA->out = D_MASK;
-	IOPORTA->dir = A_MASK;
+	// PU unused, PU on D3..D0, drive A15..A14, A8
+	IOPORTA->out = D_MASK | (1u << PA4);
+	IOPORTA->dir = A_HI_MASK;
 	
 	// PU all pins, except LED
 	IOPORTB->out = ~(1u << PB0);
 	IOPORTB->dir = (1u << PB0);
 	
-	// PU on nRAM_CS, AA14..AA13, RA18..RA14
-	IOPORTC->out = 0xFFu;
-	IOPORTC->dir = 0x00u;
+	// PU on nROM_CS, #RESET, RA17..RA14, drive A7..A6
+	IOPORTC->out = RA_MASK | nRST | nROM_CS;
+	IOPORTC->dir = A_MID_MASK;
 	
-	// PU nRST, PU unused, PU RXD
-	IOPORTD->out = nRST | ~(RAM_CS | nROM_CS) | (1u << PD0);
-	IOPORTD->dir = (1u << PD1);
+	// drive all pins
+	IOPORTD->out = 0xFFu;
+	IOPORTD->dir = 0xFFu;
 	
 	// Drive nWR, nRD, nCS
 	IOPORTE->out = nWR | nRD | nCS;
@@ -256,7 +257,7 @@ int main(void)
 	
 	sei();
 	
-	uart0Puts("MBC1 Test\n");
+	uart0Puts("MBC2 Test\n");
 	
     while(1)
     {
@@ -267,13 +268,13 @@ int main(void)
 		
 		case 'u':
 			// Unknown Pins drive test
-			IOPORTD->out |= RAM_CS;
-			IOPORTD->dir |= RAM_CS | nROM_CS;
+			IOPORTC->out &= ~nROM_CS;
+			IOPORTC->dir |= nROM_CS;
 			uart0Puts_p(0);
 			while (uart0Getch() == -1);
 			
-			IOPORTD->out &= ~RAM_CS;
-			IOPORTD->dir &= ~(RAM_CS | nROM_CS);
+			IOPORTC->out |= nROM_CS;
+			IOPORTC->dir &= ~(nROM_CS);
 			uart0Puts_p(1);
 			break;
 		
@@ -285,19 +286,19 @@ int main(void)
 			
 			for (i = 0x00u; i < 0x20u; i++) {
 				
-				writeMBC1(0x2000u, i);
+				writeMBC2(0x2100u, i);
 				
 				putAddrCS(0x0000u);
 				uart0Puts("RB 0x");
 				write_usart_hex(i);
 				uart0Puts(" RB0: ");
-				printMBC1Status();
+				printMBC2Status();
 				
 				putAddrCS(0x4000u);
 				uart0Puts("RB 0x");
 				write_usart_hex(i);
 				uart0Puts(" RB1: ");
-				printMBC1Status();
+				printMBC2Status();
 				
 			}
 			
@@ -326,148 +327,6 @@ int main(void)
 			break;
 			
 		case 'b':
-			// Test (n)RAM_CS
-			uart0Puts_p(2);
-			for (i = 0; i < sizeof(perm); i++) {
-				
-				assertRST();
-				delay_us(1);
-				deassertRST();
-				
-				writeMBC1(0x4000u, 0x01u);
-				writeMBC1(0x0000u, 0x00u);
-				writeMBC1(0x6000u, 0x00u);
-				
-				IOPORTE->out = perm[i];
-				
-				printCS_Data(&print_bothRAM_CS);
-				
-				IOPORTE->out = nWR | nRD | nCS;
-				
-			}
-			
-			uart0Puts_p(3);
-			for (i = 0; i < sizeof(perm); i++) {
-				
-				assertRST();
-				delay_us(1);
-				deassertRST();
-				
-				writeMBC1(0x4000u, 0x01u);
-				writeMBC1(0x0000u, 0x0Au);
-				writeMBC1(0x6000u, 0x00u);
-				
-				IOPORTE->out = perm[i];
-				
-				printCS_Data(&print_bothRAM_CS);
-				
-				IOPORTE->out = nWR | nRD | nCS;
-				
-			}
-			
-			uart0Puts_p(4);
-			for (i = 0; i < sizeof(perm); i++) {
-				
-				assertRST();
-				delay_us(1);
-				deassertRST();
-				
-				writeMBC1(0x4000u, 0x01u);
-				writeMBC1(0x0000u, 0x00u);
-				writeMBC1(0x6000u, 0x01u);
-				
-				IOPORTE->out = perm[i];
-				
-				printCS_Data(&print_bothRAM_CS);
-				
-				IOPORTE->out = nWR | nRD | nCS;
-				
-			}
-			
-			uart0Puts_p(5);
-			for (i = 0; i < sizeof(perm); i++) {
-				
-				assertRST();
-				delay_us(1);
-				deassertRST();
-				
-				writeMBC1(0x4000u, 0x01u);
-				writeMBC1(0x0000u, 0x0Au);
-				writeMBC1(0x6000u, 0x01u);
-				
-				IOPORTE->out = perm[i];
-				
-				printCS_Data(&print_bothRAM_CS);
-				
-				IOPORTE->out = nWR | nRD | nCS;
-				
-			}
-			
-			break;
-		
-		case 'c':
-		
-			uart0Puts_p(6);
-			
-			assertRST();
-			deassertRST();
-			
-			// Mode 4M/256k
-			writeMBC1(0x6000u, 0x01u);
-			
-			putAddr(0x4000u);
-			putData(0x02u);
-			printMBC1Status();
-			assertWR();
-			printMBC1Status();
-			deassertWR();
-			printMBC1Status();
-			floatData();
-			break;
-		
-		case 'd':
-		
-			uart0Puts_p(7);
-			
-			assertRST();
-			deassertRST();
-			
-			// Mode 4M/256k
-			writeMBC1(0x6000u, 0x01u);
-			
-			putAddr(0x4000u);
-			putData(0x02u);
-			printMBC1Status();
-			assertWR();
-			printMBC1Status();
-			putAddr(0xC000u);
-			printMBC1Status();
-			floatData();
-			deassertWR();
-			printMBC1Status();
-			break;
-			
-		case 'e':
-		
-			for (addr = 0x0000u; ; addr += 0x2000) {
-				
-				assertRST();
-				deassertRST();
-				
-				writeMBC1(addr, 0x15u);
-				putAddr(0x0000);
-				printMBC1Status();
-				putAddr(0x4000);
-				printMBC1Status();
-				
-				if (addr == 0xE000u)
-					break;
-				
-			}
-			
-			break;
-			
-		case 'f':
 		
 			// Test nROM_CS while RST assert
 			assertRST();
@@ -475,30 +334,154 @@ int main(void)
 								
 				IOPORTE->out = perm[i];
 				
-				printCS_Data(&printMBC1Status);
+				printCS_Data(&printnROM_CS);
 				
 				IOPORTE->out = nWR | nRD | nCS;
 				
 			}
 			break;
+
+		case 'c':
+	
+			// test RAM
+			uart0Puts_p(2);
+	
+			assertRST();
+			delay_us(1);
+			deassertRST();
+			
+			// Enable SRAM
+			writeMBC2(0x0000u, 0x0Au);
+			j = 0x01;
+			for (addr = 0x0000; addr < 0x0200; addr <<= 1, j++) {
+				
+				writeMBC2(0xA000 | addr, j);
+				
+				if (!addr)
+					addr = 0x0001u;
+			}
+			
+			// Disable SRAM
+			writeMBC2(0x0000u, 0x00u);
+			break;
+
+		case 'd':
+		
+			// read RAM
+			uart0Puts_p(3);
+			
+			assertRST();
+			delay_us(1);
+			deassertRST();
+
+			// Enable SRAM
+			writeMBC2(0x0000u, 0x0Au);
+			
+			for (i = 0x00; i < 0x20u; i++) {
+			
+				for (j = 0x00; j < 0x10u; j++) {
+					write_usart_hex(readMBC2(0xA000 | (i << 4) | (j)));
+					uart0Puts(" ");
+				}
+				uart0Puts("\n");
+				
+			}
+			
+			// Disable SRAM
+			writeMBC2(0x0000u, 0x00u);
+			
+			break;
+		
+		case 'e':
+				
+			// test RAM
+			uart0Puts_p(2);
+				
+			assertRST();
+			delay_us(1);
+			deassertRST();
+				
+			// Enable SRAM
+			writeMBC2(0x0000u, 0x0Au);
+				
+			for (addr = 0x0000; addr < 0x0200; addr++) {
+					
+				writeMBC2(0xA000 | addr, 0x0F);
+					
+			}
+				
+			// Disable SRAM
+			writeMBC2(0x0000u, 0x00u);
+			break;
+		
+		case 'f':
+			
+			// Enable SRAM
+			writeMBC2(0x0000u, 0x0Au);
+			
+			while ((int8_t)(i = uart0Getch()) == -1);
+			while ((int8_t)(j = uart0Getch()) == -1);
+			while ((int16_t)(addr = uart0Getch()) == -1);
+			
+			writeMBC2(0xA000 | ((i & 0x03) << 8) | j, addr & 0xFFu);
+			
+			// Disable SRAM
+			writeMBC2(0x0000u, 0x00u);
+			break;
 		
 		case 'g':
 		
+			// Test which bits influence RB0, RB1 switch
 			assertRST();
-			writeMBC1(0x4000u, 0x01u);
-			writeMBC1(0x0000u, 0x0Au);
-			writeMBC1(0x6000u, 0x00u);
+			delay_us(1);
+			deassertRST();
 			
-			for (i = 0; i < sizeof(perm); i++) {
+			writeMBC2(0x0100u, 0x15u);
+			printCS_Data(&printMBC2Status);
+			break;
+			
+		case 'h':
+			
+			for (addr = 0x0000u; ; addr += 0x4000u) {
 				
-				IOPORTE->out = perm[i];
+				assertRST();
+				delay_us(1);
+				deassertRST();
 				
-				printCS_Data(&print_bothRAM_CS);
+				uart0Puts("0x");
+				write_usart_hex(addr >> 8);
+				write_usart_hex(addr & 0xFFu);
+				uart0Puts(" /// ");
 				
-				IOPORTE->out = nWR | nRD | nCS;
+				putAddr(0x0000u);
+				writeMBC2(addr, 0x15u);
+				printMBC2Status();
+				
+				putAddr(0x4000u);
+				writeMBC2(addr, 0x15u);
+				printMBC2Status();
+				
+				addr |= 0x0100u;
+				
+				uart0Puts("0x");
+				write_usart_hex(addr >> 8);
+				write_usart_hex(addr & 0xFFu);
+				uart0Puts(" /// ");
+				
+				putAddr(0x0000u);
+				writeMBC2(addr, 0x15u);
+				printMBC2Status();
+			
+				putAddr(0x4000u);
+				writeMBC2(addr, 0x15u);
+				printMBC2Status();
+				
+				if (addr == 0xC100)
+					break;
+					
+				addr &= ~(0x0100u);
 				
 			}
-		
 			break;
 		
 		case 'L':
